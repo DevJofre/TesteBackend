ba2169f5595ba77093cbba2e6790f497d8845a8a
guilherme <guilherme@acheipneus.com.br>
2025-01-23 18:31:52 -0300

    add ProductController

--- TesteBackend/Controllers/ProductController.cs
+++ TesteBackend/Controllers/ProductController.cs
@@ -1,21 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
 using TesteBackend.Model;
+using TesteBackend.Services;
 
 namespace TesteBackend.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class ProductController : ControllerBase
+public class ProductController(ProductService productService) : ControllerBase
 {
     [HttpGet(Name = "GetProducts")]
     public IEnumerable<Product> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new Product
-            {
-                Id = index,
-                Date = DateTime.Now.AddDays(index),
-                Name = $"Product {index}",
-            })
-            .ToArray();
+        return productService.GetAll();
     }
 }
