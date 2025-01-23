25650b6daedd2a751e90a2db53bc87e3495efda9
guilherme <guilherme@acheipneus.com.br>
2025-01-23 18:31:23 -0300

    add Services/ model and Product service

---
+++ TesteBackend/Services/ProductService.cs
@@ -0,0 +1,18 @@
+using System.Diagnostics;
+using TesteBackend.Model;
+
+namespace TesteBackend.Services;
+
+public class ProductService
+{
+    public IEnumerable<Product> GetAll()
+    {
+        return Enumerable.Range(1, 5).Select(index => new Product
+            {
+                Id = index,
+                Date = DateTime.Now.AddDays(index),
+                Name = $"Product {index}",
+            })
+            .ToArray();
+    }
+}
