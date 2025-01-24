2f8dd20b95e02fe36ccfe2d13d628a456f44f9d5
guilherme <guilherme@acheipneus.com.br>
2025-01-24 15:30:04 -0300

    migration for product Date -> DateCreated

---
+++ TesteBackend/Migrations/20250124181916_productDateCreated.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TesteBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class productDateCreated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "Product",
+                newName: "DateCreated");
+
+            migrationBuilder.UpdateData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "Name" },
+                values: new object[] { new DateTime(2025, 1, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "IPhone X" });
+
+            migrationBuilder.UpdateData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "Name" },
+                values: new object[] { new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "IPhone IX" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateCreated",
+                table: "Product",
+                newName: "Date");
+
+            migrationBuilder.UpdateData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Date", "Name" },
+                values: new object[] { new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "IPhone IX" });
+
+            migrationBuilder.UpdateData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Date", "Name" },
+                values: new object[] { new DateTime(2025, 1, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "IPhone X" });
+        }
+    }
+}
