5f649e31fc4aa9a9357d4adca78c7d4400114547
guilherme <guilherme@acheipneus.com.br>
2025-01-24 14:24:28 -0300

    migration: product seed

---
+++ TesteBackend/Migrations/20250124172248_ProductSeed.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TesteBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Product",
+                columns: new[] { "Id", "Date", "Name" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "IPhone IX" },
+                    { 2, new DateTime(2025, 1, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "IPhone X" },
+                    { 3, new DateTime(2025, 1, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), "IPhone XI" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
--- TesteBackend/Models/Configuration/ProductConfiguration.cs
+++ TesteBackend/Models/Configuration/ProductConfiguration.cs
@@ -13,5 +13,10 @@ internal sealed class ProductConfiguration: IEntityTypeConfiguration<Product>
         builder.Property(pt => pt.Name).HasMaxLength(100);
 
         /* seed de valores */
+        builder.HasData(
+            new Product() { Id = 1, Name = "IPhone X", Date = DateTime.Parse("2025-01-02")},
+            new Product() { Id = 2, Name = "IPhone IX", Date = DateTime.Parse("2025-01-01")},
+            new Product() { Id = 3, Name = "IPhone XI", Date = DateTime.Parse("2025-01-03")}
+        );
     }
 }
