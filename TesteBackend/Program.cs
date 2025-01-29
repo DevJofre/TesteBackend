708f7741e3106d1a26b3f6218d917cd53324d07a
DevJofre <jofretomas9@gmail.com>
2025-01-29 16:05:17 -0300

    Created the logging structure in the project

--- TesteBackend/Program.cs
+++ TesteBackend/Program.cs
@@ -1,8 +1,23 @@
 using TesteBackend;
 using TesteBackend.Services;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+var logDirectory = Path.Combine(Directory.GetCurrentDirectory(), "logs");
+if (!Directory.Exists(logDirectory))
+{
+    Directory.CreateDirectory(logDirectory);
+}
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
 
 // Add services to the container.
 
@@ -22,6 +37,9 @@ builder.Services.AddScoped<ProductService>();
 builder.Services.AddScoped<CategoryService>();
 
 var app = builder.Build();
+
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+logger.LogInformation("🚀 A aplicação iniciou! Esse log deve aparecer no arquivo logs/log.txt");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
